51a3835391e23ddce2eb92de5b5cbd802ec70267
PsypherPunk <psypherpunk@gmail.com>
2016-01-31 09:57:41 +0000

    More tests.

--- annotation/test_views.py
+++ annotation/test_views.py
@@ -2,6 +2,7 @@ import json
 from django.core.urlresolvers import reverse
 from django.test import TestCase
 from django.test.client import RequestFactory
+from urllib.parse import urljoin
 from annotation import views
 
 
@@ -10,6 +11,8 @@ class IndexTest(TestCase):
     def setUp(self):
         self.factory = RequestFactory()
         self.index_create_url = reverse("index_create")
+        self.read_update_delete_url = reverse("read_update_delete",
+                                              kwargs={"pk": None})
         self.annotation = {
             "annotator_schema_version": "v1.0",
             "text": "A note I wrote",
@@ -25,20 +28,40 @@ class IndexTest(TestCase):
             ]
         }
 
+    def create_annotation(self):
+        request = self.factory.post(self.index_create_url,
+                                    data=json.dumps(self.annotation),
+                                    content_type="application/json")
+        response = views.index_create(request)
+        return response
+        
+
     def test_index_create(self):
         request = self.factory.get(self.index_create_url)
         response = views.index_create(request)
         content = json.loads(response.content.decode("utf-8"))
         self.assertEquals(0, len(content))
 
-        request = self.factory.post(self.index_create_url,
-                                    data=json.dumps(self.annotation),
-                                    content_type="application/json")
-        response = views.index_create(request)
+        response = self.create_annotation()
         self.assertEquals(303, response.status_code)
         self.assertTrue(response.has_header("Location"))
-        print(response.get("Location"))
-        #content = json.loads(response.content.decode("utf-8"))
-        # Verify that the created annotation is identical...
-        #for key in self.annotation.keys():
-        #    self.assertEquals(content.get(key), self.annotation.get(key))
+
+        request = self.factory.get(self.index_create_url)
+        response = views.index_create(request)
+        content = json.loads(response.content.decode("utf-8"))
+        self.assertEquals(1, len(content))
+
+    def test_read_update_delete(self):
+        self.create_annotation()
+        request = self.factory.get(self.index_create_url)
+        response = views.index_create(request)
+        content = json.loads(response.content.decode("utf-8"))
+
+        request =  self.factory.get(self.read_update_delete_url)
+        response = views.read_update_delete(request,
+                                            content[0].get("id"))
+        content = json.loads(response.content.decode("utf-8"))
+
+        for key in self.annotation.keys():
+            self.assertEquals(content.get(key), self.annotation.get(key))
+
