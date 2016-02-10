48fc8e90b38ff5fd956ec18ee12eb1bfdad5d860
Roger G. Coram <roger@thedatashed.co.uk>
2016-02-10 10:11:37 +0000

    Include `django-cors-headers` to avoid `Access-Control-Allow-Origin`.

--- annotator/settings/base.py
+++ annotator/settings/base.py
@@ -39,10 +39,12 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'rest_framework',
     'annotation',
+    'corsheaders',
 )
 
 MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -102,3 +104,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CORS_ORIGIN_ALLOW_ALL = True
+
